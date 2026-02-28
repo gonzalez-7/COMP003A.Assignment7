ca0c36548f3476d3b12197558a1b70b2e46e6176
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-27 19:47:24 -0800

    Added score collection with initial values

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace COMP003A.Assignment7
+﻿using System.Collections.Generic;
+
+namespace COMP003A.Assignment7
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			// The purpose of this program is to
+			// store and analyze the student scores using a repeating menu
+			//Collection Declaration -
+
+			List<int> scores = new List<int>() { 85, 90, 78, 92, 88 };
 		}
 	}
 }
